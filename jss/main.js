4fe5b1eb9128e5a7acd529385731b5eedbee093f
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-15 18:29:09 +0200

    finito

--- jss/main.js
+++ jss/main.js
@@ -1,41 +1,55 @@
 let randomNumbers = document.getElementById('rNumbers');
+let numeriGiusti = document.getElementById('numeriGiusti')
 let arrayNumb = [];
 let numeriInseriti = []
 let numeroInserito;
+
 
 for (let i=0 ; i<5 ; i++){
     let generatoreNumeri = Math.floor(Math.random()*100);
     arrayNumb.push(generatoreNumeri);
     while(!arrayNumb.includes(generatoreNumeri)){
-
         let generatoreNumeri = Math.floor(Math.random()*100);
         arrayNumb.push(generatoreNumeri);
-
     }
 }
+
+    randomNumbers.append(arrayNumb);
 
 // let finalNmbArray = randomNumbers.append(arrayNumb)
 console.log(arrayNumb);
 
-for(let i=0 ; i<5 ; i++){
+setTimeout(function(){
 
-    numeroInserito = parseInt(prompt('Inserisci un Valore'));
+    randomNumbers.style.display = 'none';
+},2000)
 
-    if(arrayNumb.includes(numeroInserito)){
+setTimeout(function() {
 
-        numeriInseriti.push(parseInt(numeroInserito));
-        console.log('si')
+    
+
+    for(let i=0 ; i<5 ; i++){
+
+        numeroInserito = parseInt(prompt('Inserisci un Valore'));
+    
+        if(arrayNumb.includes(numeroInserito)){
+    
+            numeriInseriti.push(parseInt(numeroInserito));
+            console.log('si')
+        }
+    
+        else {
+    
+            console.log('no');
+        }
+    
+        console.log(numeriInseriti);
     }
+    
+    numeriGiusti.append(numeriInseriti);
 
-    else {
+},3000)
 
-        console.log('no');
-    }
-
-    console.log(numeriInseriti);
-}
-
-randomNumbers.append(numeriInseriti);
 
 
 // for (i=0 ; i<arrayNumb.length ; i++) {
