26726fd900231d08cff6d10424286377183708b5
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-15 15:21:30 +0200

    Array Numeri Inseriti

--- jss/main.js
+++ jss/main.js
@@ -1,5 +1,6 @@
 let randomNumbers = document.getElementById('rNumbers');
 let arrayNumb = [];
+let numeriInseriti = []
 
 for (i=0 ; i<6 ; i++){
     let generatoreNumeri = Math.floor(Math.random()*100);
@@ -13,4 +14,6 @@ randomNumbers.append(arrayNumb)
 for(i=0 ; i<6 ; i++){
 
     let numeroInserito = prompt('Inserisci un Valore');
+    numeriInseriti.push(numeroInserito);
+    console.log(numeriInseriti);
 }
