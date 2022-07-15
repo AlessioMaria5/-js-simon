af6b1c1a182f9d7cbd07260cd26a642284e2e767
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-15 16:08:29 +0200

    bloccato su check degli arrays

--- jss/main.js
+++ jss/main.js
@@ -2,18 +2,30 @@ let randomNumbers = document.getElementById('rNumbers');
 let arrayNumb = [];
 let numeriInseriti = []
 
-for (i=0 ; i<6 ; i++){
+for (let i=0 ; i<6 ; i++){
     let generatoreNumeri = Math.floor(Math.random()*100);
     arrayNumb.push(generatoreNumeri);
 }
 
-randomNumbers.append(arrayNumb)
+let finalNmbArray = randomNumbers.append(arrayNumb)
+console.log(arrayNumb);
 
-
-
-for(i=0 ; i<6 ; i++){
+for(let i=0 ; i<6 ; i++){
 
     let numeroInserito = prompt('Inserisci un Valore');
-    numeriInseriti.push(numeroInserito);
-    console.log(numeriInseriti);
+    numeriInseriti.push(parseInt(numeroInserito));
+
+    if(numeroInserito.includes(arrayNumb){
+
+        console.log('ciao')
+    }
+
+    else {
+
+        console.log('no');
+    }
 }
+
+
+// for (i=0 ; i<arrayNumb.length ; i++) {
+// }
