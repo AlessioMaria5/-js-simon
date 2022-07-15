14d579ecf4d4c117cdc51528075b250741a13e11
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-15 17:57:07 +0200

    push finito senza timer

--- jss/main.js
+++ jss/main.js
@@ -1,30 +1,41 @@
 let randomNumbers = document.getElementById('rNumbers');
 let arrayNumb = [];
 let numeriInseriti = []
+let numeroInserito;
 
-for (let i=0 ; i<6 ; i++){
+for (let i=0 ; i<5 ; i++){
     let generatoreNumeri = Math.floor(Math.random()*100);
     arrayNumb.push(generatoreNumeri);
+    while(!arrayNumb.includes(generatoreNumeri)){
+
+        let generatoreNumeri = Math.floor(Math.random()*100);
+        arrayNumb.push(generatoreNumeri);
+
+    }
 }
 
-let finalNmbArray = randomNumbers.append(arrayNumb)
+// let finalNmbArray = randomNumbers.append(arrayNumb)
 console.log(arrayNumb);
 
-for(let i=0 ; i<6 ; i++){
+for(let i=0 ; i<5 ; i++){
 
-    let numeroInserito = prompt('Inserisci un Valore');
-    numeriInseriti.push(parseInt(numeroInserito));
+    numeroInserito = parseInt(prompt('Inserisci un Valore'));
 
-    if(numeroInserito.includes(arrayNumb){
+    if(arrayNumb.includes(numeroInserito)){
 
-        console.log('ciao')
+        numeriInseriti.push(parseInt(numeroInserito));
+        console.log('si')
     }
 
     else {
 
         console.log('no');
     }
+
+    console.log(numeriInseriti);
 }
+
+randomNumbers.append(numeriInseriti);
 
 
 // for (i=0 ; i<arrayNumb.length ; i++) {
